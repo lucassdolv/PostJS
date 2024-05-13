fb490cfff28ad3d0979a25b5bd8350b6e73051a5
lucassdolv <lucassdo2003@gmail.com>
2024-05-13 16:40:01 -0300

    first

---
+++ scriptLog.js
@@ -0,0 +1,25 @@
+const formLog = document.getElementById("formLog");
+
+formLog.addEventListener("submit", (e)=>{
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
+
+    e.preventDefault();
+    
+    const userData =  JSON.stringify({
+        email:email.value,
+        password:password.value
+    })
+
+    fetch("http://10.92.198.38:8080/auth/signin", {
+        method: "Post",
+        body: userData,
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+        })
+    .then((result) => result.json())
+    .then((data) => console.log(data))
+    .then((err) => console.log(err))
+    
+        console.log(userData)
+
+})
